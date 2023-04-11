c8e6cc5288e83bcd4660b6a6a7c30a062a21305d
irakusaReo <129947595+irakusaReo@users.noreply.github.com>
2023-04-11 15:39:54 +0300

    Add files via upload
    Complete program. 
    Sifts through images in my computer.
    Finds the corresponding record on AirTable.
    Updates file name.
    Uses chatGPT to generate long and short descriptions to use on our ecommerce site after collecting its name in the table.

--- test6.py
+++ test6.py
@@ -5,19 +5,19 @@ import openai
 import time
 
 # Set up Airtable API client
-AIRTABLE_API_KEY = "private"
-AIRTABLE_BASE_KEY = "and"
-AIRTABLE_TABLE_NAME = "confidential"
+AIRTABLE_API_KEY = "keyeDQ87k9Looanna"
+AIRTABLE_BASE_KEY = "appOo3NIs0nT86pYB"
+AIRTABLE_TABLE_NAME = "Formulas Test"
 airtable_client = airtable.Airtable(AIRTABLE_BASE_KEY, AIRTABLE_TABLE_NAME, AIRTABLE_API_KEY)
 
 # Set up OpenAI API client
-OPENAI_API_KEY = "also_private"
+OPENAI_API_KEY = "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"
 openai.api_key = OPENAI_API_KEY
 
 # Define function to generate a description using OpenAI API
 
 def generate_description(item_name):
-    prompt = f"Write me a description, in a paragraph of at least 100 words, to be used on an ecommerce platform for {item_name}. Refrain from referring to the name of the item in the beginning of the paragraph."
+    prompt = f"Write me a description, in a paragraph of at least 100 words, to be used on an ecommerce platform for {item_name}. Do not mention the name of the item."
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=prompt,
@@ -35,7 +35,7 @@ def generate_description(item_name):
 
 # Define function to generate bullet points using OpenAI API
 def generate_bullet_points(description):
-    prompt = f"Generate short points for:\n{description}. Using at least 5 bullets. Each bullet should have a maximum of 20 words."
+    prompt = f"Generate short points for:\n{description}. Using at least 5 bullets. Each bullet should have a maximum of 10 words. Use this bullet:'•'. Do not mention the name of the item. Include dimensions if they're mentioned in the description or item name:{item_name}"
     completions = openai.Completion.create(
         engine="text-davinci-002",
         prompt=prompt,
@@ -44,9 +44,11 @@ def generate_bullet_points(description):
         stop=None,
         temperature=0.5,
         )
+    
     if completions.choices[0].text.strip():
         bullet_points = completions.choices[0].text.strip()
-        bullet_points = "• " + bullet_points.replace("\n", "\n• ")
+        bullet_points = [bp.strip() for bp in bullet_points.split("\n")]
+        bullet_points = "\n".join(bullet_points)
         print("Bullets, done!")
         return bullet_points
     else:
@@ -54,7 +56,7 @@ def generate_bullet_points(description):
         return None
 
 # Define folder path
-FOLDER_PATH = r"C:\Users\Andreo\Desktop\AutomationTestData\AutomationTestData2"
+FOLDER_PATH = r"C:\Users\Andreo\Desktop\AutomationTestData\Batch 120"
 
 # Define time variables for rate limiting
 start_time = time.time()
@@ -82,27 +84,42 @@ for filename in os.listdir(FOLDER_PATH):
         request_count = 0
 
     if filename.endswith(".jpg"):
-        # Get item number from file name
-        match = re.search(r"^(.*?)_\d+\.jpg$", filename)
+        # Get item number and image number from file name
+        match = re.search(r"^(.*)_([0-9]+)\.jpg$", filename)
         if match:
             item_number = match.group(1)
+            image_number = int(match.group(2))
         else:
-            print(f"Error: could not extract item number from file name {filename}")
+            print(f"Error: could not extract item and image numbers from file name {filename}")
+            continue
+        
+        # Determine which Airtable field to update based on the image number
+        if image_number >= 1 and image_number <= 11:
+            airtable_field = f"Image {image_number}"
+        else:
+            print(f"Error: invalid image number {image_number} for file {filename}")
             continue
         # Search for item in Airtable using item number
         records = airtable_client.search("ItemNo+Col", item_number)
         # If item is found, update TACC Web Status and generate description and bullet points
         if records:
             record = records[0]
-            airtable_client.update(record["id"], {"TACC Web Status": "117"})
-            item_name = record["fields"].get("B2C Product Title") or record["fields"].get("Product Name")
+            airtable_client.update(record["id"], {"TACC Web Status": "120"})
+            item_name = record["fields"].get("B2C Product Title") or record["fields"].get("Product name")
             if item_name:
-                description = generate_description(item_name)
-                bullet_points = generate_bullet_points(description)
-                if description and bullet_points:
-                    airtable_client.update(record["id"], {"B2C Long Description": description, "B2C Short Description": bullet_points})
-                    print(f"Generated description and bullet points for item number {item_number}")
+                description = record["fields"].get("B2C Long Description")
+                bullet_points = record["fields"].get("B2C Short Description")
+                if not description or not bullet_points:
+                    description = generate_description(item_name)
+                    bullet_points = generate_bullet_points(description)
+                    if description and bullet_points:
+                        airtable_client.update(record["id"], {"B2C Long Description": description, "B2C Short Description": bullet_points, airtable_field:filename})
+                        print(f"Generated description and bullet points for item number {item_number}")
+                else:
+                    print(f"B2C Long Description and B2C Short Description fields are not empty for item number {item_number}")
             else:
                 print(f"No B2C Product Title or Product Name found for item number {item_number}")
         else:
             print(f"No record found for item number {item_number}")
+
+            
